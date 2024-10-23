21ae7bd0923c9bf202e1b1873cdb85183f407bbc
Alexander Mandrov <otvet-1995@mail.ru>
2024-10-23 17:57:24 +0300

    add favicon, title, update some mobile styles, add debounce on search filter

--- src/pages/users/user-row.tsx
+++ src/pages/users/user-row.tsx
@@ -13,6 +13,7 @@ import {
   FormControl,
   FormField,
   FormItem,
+  FormLabel,
   FormMessage,
 } from "@/components/ui/form";
 import { Button } from "@/components/ui/button";
@@ -28,14 +29,19 @@ type UserRowProps = {
 
 export const UserRow = ({ formControl, index, onRemove }: UserRowProps) => {
   return (
-    <div className="grid grid-cols-[1fr_1fr_1fr_1fr_auto] gap-4 p-4 border-t items-center">
+    <div className="grid grid-cols-[1fr_1fr_1fr_1fr_auto] gap-4 p-4 border-t items-start max-sm:grid-cols-none">
       <FormField
         control={formControl}
         name={`users.${index}.name`}
         render={({ field }) => (
           <FormItem>
+            <FormLabel className="max-sm:flex hidden">Name</FormLabel>
             <FormControl>
-              <Input {...field} placeholder="Name" />
+              <Input
+                defaultValue={field.value}
+                onChange={field.onChange}
+                placeholder="Name"
+              />
             </FormControl>
             <FormMessage />
           </FormItem>
@@ -46,6 +52,7 @@ export const UserRow = ({ formControl, index, onRemove }: UserRowProps) => {
         name={`users.${index}.country`}
         render={({ field }) => (
           <FormItem>
+            <FormLabel className="max-sm:flex hidden">Country</FormLabel>
             <Select onValueChange={field.onChange}>
               <FormControl>
                 <SelectTrigger>
@@ -69,8 +76,13 @@ export const UserRow = ({ formControl, index, onRemove }: UserRowProps) => {
         name={`users.${index}.email`}
         render={({ field }) => (
           <FormItem>
+            <FormLabel className="max-sm:flex hidden">Email</FormLabel>
             <FormControl>
-              <Input {...field} placeholder="Email" />
+              <Input
+                defaultValue={field.value}
+                onChange={field.onChange}
+                placeholder="example@email.com"
+              />
             </FormControl>
             <FormMessage />
           </FormItem>
@@ -81,8 +93,13 @@ export const UserRow = ({ formControl, index, onRemove }: UserRowProps) => {
         name={`users.${index}.phone`}
         render={({ field }) => (
           <FormItem>
+            <FormLabel className="max-sm:flex hidden">Phone</FormLabel>
             <FormControl>
-              <Input {...field} placeholder="Phone" />
+              <Input
+                defaultValue={field.value}
+                onChange={field.onChange}
+                placeholder="Phone"
+              />
             </FormControl>
             <FormMessage />
           </FormItem>
@@ -93,7 +110,7 @@ export const UserRow = ({ formControl, index, onRemove }: UserRowProps) => {
         variant="ghost"
         size="icon"
         onClick={onRemove}
-        className="text-red-500 hover:text-red-700"
+        className="text-red-500 hover:text-red-700 max-sm:ml-auto mt-2"
       >
         <Trash2 className="w-5 h-5" />
       </Button>
--- src/pages/users/users-list.tsx
+++ src/pages/users/users-list.tsx
@@ -1,7 +1,8 @@
-import { useState } from "react";
-// import { v4 as uuidv4 } from "uuid";
-import { Plus } from "lucide-react";
+import { useEffect, useMemo, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { UserRoundPlus } from "lucide-react";
 import { useForm, useFieldArray } from "react-hook-form";
+import debounce from "debounce";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 import { Button } from "@/components/ui/button";
@@ -11,11 +12,10 @@ import useUsersContext from "@/hooks/useUsersContext";
 import { setStorageItem } from "@/lib/utils";
 import { LS_USERS_KEY } from "@/lib/constants";
 import { formSchema } from "@/lib/schemas";
-import { FormType } from "@/lib/types";
+import { FormType, User } from "@/lib/types";
 import SkeletonRows from "@/components/skeleton-rows";
 import { UserRow } from "./user-row";
 import { Form } from "@/components/ui/form";
-// import { UserRow } from "./user-row";
 
 // TODO: explain why RHF (useRef instead of useState) is better for decreasing re-renders
 
@@ -31,80 +31,92 @@ export default function UserList() {
   const {
     control,
     formState: { errors, dirtyFields },
-    // reset,
-    // watch,
+    reset,
+    watch,
   } = form;
 
-  const {
-    fields,
-    remove,
-    //  prepend
-  } = useFieldArray({
+  const { fields, remove, prepend } = useFieldArray({
     control,
     name: "users",
   });
 
   const onSubmit = (values: FormType) => {
     setStorageItem(LS_USERS_KEY, values.users);
-    // reset(values, { keepValues: true });
+    reset(values, { keepValues: true });
   };
 
-  // const handleAddUser = () => {
-  //   prepend({
-  //     id: uuidv4(),
-  //     name: "",
-  //     country: "Israel",
-  //     email: "",
-  //     phone: "",
-  //   });
-  // };
+  const handleAddUser = () => {
+    prepend({
+      id: uuidv4(),
+      name: "",
+      country: "Israel",
+      email: "",
+      phone: "",
+    });
+  };
 
-  // const watchedUsers = watch("users");
+  const watchedUsers = watch("users");
 
-  // const errorCounts = watchedUsers.reduce(
-  //   (acc, user, index) => {
-  //     Object.keys(user).forEach((key) => {
-  //       if (key !== "id" && errors.users?.[index]?.[key as keyof User]) {
-  //         if (user[key as keyof User] === "") {
-  //           acc.emptyFields++;
-  //         } else {
-  //           acc.invalidFields++;
-  //         }
-  //       }
-  //     });
-  //     return acc;
-  //   },
-  //   { emptyFields: 0, invalidFields: 0 }
-  // );
+  const errorCounts = watchedUsers.reduce(
+    (acc, user, index) => {
+      Object.keys(user).forEach((key) => {
+        if (key !== "id" && errors.users?.[index]?.[key as keyof User]) {
+          if (user[key as keyof User] === "") {
+            acc.emptyFields++;
+          } else {
+            acc.invalidFields++;
+          }
+        }
+      });
+      return acc;
+    },
+    { emptyFields: 0, invalidFields: 0 }
+  );
 
-  // const filteredFields = fields.filter((field) =>
-  //   Object.values(field).some((value) =>
-  //     value.toString().toLowerCase().includes(searchTerm.toLowerCase())
-  //   )
-  // );
+  const filteredFields = fields.filter((field) =>
+    Object.values(field).some((value) => {
+      const res = value
+        .toString()
+        .toLowerCase()
+        .includes(searchTerm.toLowerCase());
+      if (res) {
+        // console.log("true, field, value", field, value);
+      }
+      return res;
+    })
+  );
+
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // console.log("setting searchTerm to: ", e.target.value);
+    setSearchTerm(e.target.value);
+  };
+
+  const debouncedSearch = useMemo(() => {
+    return debounce(handleSearch, 300);
+  }, []);
+
+  useEffect(() => {
+    return () => {
+      debouncedSearch.clear();
+    };
+  });
 
   return (
     <div className="p-4">
-      <div className="flex justify-between items-center mb-4">
-        {/* <h2 className="text-2xl font-bold">Users List ({fields.length})</h2> */}
-        <Button
-          // onClick={handleAddUser}
-          className="flex items-center gap-2"
-        >
-          <Plus className="w-4 h-4" /> Add User
-        </Button>
+      <div className="flex flex-wrap gap-4 justify-between items-center mb-4">
+        <h2 className="text-2xl font-bold">Users List ({fields.length})</h2>
+        <Input
+          type="text"
+          placeholder="Search users..."
+          onChange={debouncedSearch}
+          className="max-w-xs"
+        />
       </div>
-      <Input
-        type="text"
-        placeholder="Search users..."
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-        className="mb-4"
-      />
-      {/* <div className="mt-4 mb-4">
+
+      <div className="mt-4 mb-4">
         Errors: Empty Fields - {errorCounts.emptyFields}, Invalid Fields -{" "}
         {errorCounts.invalidFields}
-      </div> */}
+      </div>
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)}>
           <Button
@@ -118,17 +130,24 @@ export default function UserList() {
             Save Changes
           </Button>
           <div className="border rounded-lg overflow-hidden">
-            <div className="grid grid-cols-[1fr_1fr_1fr_1fr_auto] gap-4 bg-gray-100 p-4 font-semibold">
-              <div>Name</div>
-              <div>Country</div>
-              <div>Email</div>
-              <div>Phone</div>
-              <div></div>
+            <div className="grid grid-cols-[1fr_1fr_1fr_1fr_auto] gap-4 bg-gray-100 p-4 font-semibold items-center max-sm:grid-cols-[1fr_1fr]">
+              <div className="hidden max-sm:flex">Users Table</div>
+              <div className="ml-2 max-sm:hidden">Name</div>
+              <div className="ml-2 max-sm:hidden">Country</div>
+              <div className="ml-2 max-sm:hidden">Email</div>
+              <div className="ml-2 max-sm:hidden">Phone</div>
+              <Button
+                onClick={handleAddUser}
+                className="p-3 max-sm:ml-auto"
+                variant="outline"
+              >
+                <UserRoundPlus className="w-4 h-4" />
+              </Button>
             </div>
             {isLoading ? (
               <SkeletonRows />
             ) : (
-              fields.map((field, index) => (
+              filteredFields.map((field, index) => (
                 <UserRow
                   key={field.id}
                   index={index}
