7e4b6df9de9dc5febb7f12c2375be62234de0f7a
Alexander Mandrov <otvet-1995@mail.ru>
2024-10-25 02:50:07 +0300

    +

--- src/pages/users/components/user-row.tsx
+++ src/pages/users/components/user-row.tsx
@@ -19,7 +19,6 @@ import { Button } from "@/components/ui/button";
 import { FormType } from "@/lib/types";
 import { emailSchema, nameSchema, phoneSchema } from "@/lib/schemas";
 import { AwesomeInput } from "@/pages/users/components/awesome-input";
-import { memo } from "react";
 
 type UserRowProps = {
   index: number;
@@ -28,105 +27,108 @@ type UserRowProps = {
   formControl: Control<FormType>;
 };
 
-export const UserRow = memo(
-  ({ index, onRemove, style, formControl }: UserRowProps) => {
-    const { getValues, setValue } = useFormContext();
+export const UserRow = ({
+  index,
+  onRemove,
+  style,
+  formControl,
+}: UserRowProps) => {
+  const { getValues, setValue } = useFormContext();
 
-    const nameKey = `users.${index}.name` as const;
-    const countryKey = `users.${index}.country` as const;
-    const emailKey = `users.${index}.email` as const;
-    const phoneKey = `users.${index}.phone` as const;
+  const nameKey = `users.${index}.name` as const;
+  const countryKey = `users.${index}.country` as const;
+  const emailKey = `users.${index}.email` as const;
+  const phoneKey = `users.${index}.phone` as const;
 
-    return (
-      <div
-        style={style}
-        className="grid grid-cols-[1fr_1fr_1fr_1fr_auto] gap-4 p-4 border-t items-start max-sm:grid-cols-none"
+  return (
+    <div
+      style={style}
+      className="grid grid-cols-[1fr_1fr_1fr_1fr_auto] gap-4 p-4 border-t items-start max-sm:grid-cols-none"
+    >
+      <FormField
+        control={formControl}
+        name={nameKey}
+        render={() => (
+          <FormItem>
+            <FormLabel className="max-sm:flex hidden">Name</FormLabel>
+            <FormControl>
+              <AwesomeInput
+                inputKey={nameKey}
+                schema={nameSchema}
+                placeholder="Name"
+              />
+            </FormControl>
+          </FormItem>
+        )}
+      />
+
+      <FormField
+        control={formControl}
+        name={countryKey}
+        render={() => (
+          <FormItem>
+            <FormLabel className="max-sm:flex hidden">Country</FormLabel>
+            <Select onValueChange={(str) => setValue(countryKey, str)}>
+              <FormControl>
+                <SelectTrigger>
+                  <SelectValue placeholder={getValues(countryKey)} />
+                </SelectTrigger>
+              </FormControl>
+              <SelectContent>
+                {countries.map((country) => (
+                  <SelectItem key={country} value={country}>
+                    {country}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+          </FormItem>
+        )}
+      />
+
+      <FormField
+        control={formControl}
+        name={emailKey}
+        render={() => (
+          <FormItem>
+            <FormLabel className="max-sm:flex hidden">Email</FormLabel>
+            <FormControl>
+              <AwesomeInput
+                inputKey={emailKey}
+                schema={emailSchema}
+                placeholder="example@email.com"
+              />
+            </FormControl>
+          </FormItem>
+        )}
+      />
+
+      <FormField
+        control={formControl}
+        name={phoneKey}
+        render={() => (
+          <FormItem>
+            <FormLabel className="max-sm:flex hidden">Phone</FormLabel>
+            <FormControl>
+              <AwesomeInput
+                inputKey={phoneKey}
+                schema={phoneSchema}
+                placeholder="Phone"
+              />
+            </FormControl>
+          </FormItem>
+        )}
+      />
+
+      <Button
+        type="button"
+        variant="ghost"
+        size="icon"
+        onClick={onRemove}
+        className="text-red-500 hover:text-red-700 max-sm:ml-auto mt-2"
       >
-        <FormField
-          control={formControl}
-          name={nameKey}
-          render={() => (
-            <FormItem>
-              <FormLabel className="max-sm:flex hidden">Name</FormLabel>
-              <FormControl>
-                <AwesomeInput
-                  inputKey={nameKey}
-                  schema={nameSchema}
-                  placeholder="Name"
-                />
-              </FormControl>
-            </FormItem>
-          )}
-        />
-
-        <FormField
-          control={formControl}
-          name={countryKey}
-          render={() => (
-            <FormItem>
-              <FormLabel className="max-sm:flex hidden">Country</FormLabel>
-              <Select onValueChange={(str) => setValue(countryKey, str)}>
-                <FormControl>
-                  <SelectTrigger>
-                    <SelectValue placeholder={getValues(countryKey)} />
-                  </SelectTrigger>
-                </FormControl>
-                <SelectContent>
-                  {countries.map((country) => (
-                    <SelectItem key={country} value={country}>
-                      {country}
-                    </SelectItem>
-                  ))}
-                </SelectContent>
-              </Select>
-            </FormItem>
-          )}
-        />
-
-        <FormField
-          control={formControl}
-          name={emailKey}
-          render={() => (
-            <FormItem>
-              <FormLabel className="max-sm:flex hidden">Email</FormLabel>
-              <FormControl>
-                <AwesomeInput
-                  inputKey={emailKey}
-                  schema={emailSchema}
-                  placeholder="example@email.com"
-                />
-              </FormControl>
-            </FormItem>
-          )}
-        />
-
-        <FormField
-          control={formControl}
-          name={phoneKey}
-          render={() => (
-            <FormItem>
-              <FormLabel className="max-sm:flex hidden">Phone</FormLabel>
-              <FormControl>
-                <AwesomeInput
-                  inputKey={phoneKey}
-                  schema={phoneSchema}
-                  placeholder="Phone"
-                />
-              </FormControl>
-            </FormItem>
-          )}
-        />
-
-        <Button
-          type="button"
-          variant="ghost"
-          size="icon"
-          onClick={onRemove}
-          className="text-red-500 hover:text-red-700 max-sm:ml-auto mt-2"
-        >
-          <Trash2 className="w-5 h-5" />
-        </Button>
-      </div>
-    );
-  }
-);
+        <Trash2 className="w-5 h-5" />
+      </Button>
+    </div>
+  );
+};
